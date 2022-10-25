b1c4b7656521b09d695e2c66a7dbddb4eb03ef32
이성훈 <lgshceo@naver.com>
2022-10-25 17:51:44 +0900

    feat: 🎸 #6 상세 - 메뉴 작업중

---
+++ frontend/src/components/customer/BackButtonHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="header">
+        <img src="@/assets/Back-Navs.svg" style="width:2.5rem;" alt />
+        <img src="@/assets/Home.svg" style="width:2.5rem;" alt />
+        <img src="@/assets/Cart.svg" style="width:2.5rem;" alt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/customer/OrderView.vue
+++ frontend/src/views/customer/OrderView.vue
@@ -1,5 +1,29 @@
 <template>
+<Header></Header>
+<div class="container">
+
     <div>
       <h1>상세 - 메뉴</h1>
+      <div v-if="tapState === 1">tap1</div>
+      <div v-if="tapState === 2">tap2</div>
+      <div v-if="tapState === 3">tap3</div>
+      <button @click="tapState = 1">Button1</button>
+      <button @click="tapState = 2">Button2</button>
+      <button @click="tapState = 3">npButton3</button>
     </div>
-  </template>
+</div>
+</template>
+
+<script>
+import Header from '@/components/customer/BackButtonHeader.vue'
+export default {
+    components: {
+        Header,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
