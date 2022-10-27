bbfc25ff32781f9d6a402100589673e5d0941ea9
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-10-27 09:42:30 +0900

    feat: #11 OrdersService - 어노테이션 추가

---
+++ foodtruck/src/main/java/com/ssafy/foodtruck/model/service/OrdersService.java
@@ -0,0 +1,10 @@
+package com.ssafy.foodtruck.model.service;
+
+import com.ssafy.foodtruck.dto.RegisterOrdersRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OrdersService {
+}
