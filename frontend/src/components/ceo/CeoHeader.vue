0ab69c4da2a38b8f5dd199552b389a92c7582b37
ggpp200 <ggpp20000@gmail.com>
2022-10-27 15:31:44 +0900

    feat: 🎸 #14 사장님 header2 추가

---
+++ frontend/src/components/ceo/CeoHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header2">
+    <img src="@/assets/ceo/nav2Back.svg" alt />
+    <img src="@/assets/markerIcon.svg" alt />
+    <address style="margin:auto;   font-style: normal;">주소</address>
+    <img src="@/assets/noticeIcon.svg" alt />
+    <img src="@/assets/humanIcon.svg" style="margin-left:1rem;" alt />
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header2 {
+  font-family: "SCoreDream";
+  display: flex;
+  width: 90%;
+  margin: 5%;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- frontend/src/views/ceo/StartView.vue
+++ frontend/src/views/ceo/StartView.vue
@@ -1,9 +1,15 @@
 <template>
+    <CeoHeader></CeoHeader>
+
     <h1>영업시작 페이지</h1>
 </template>
 
 <script>
+import CeoHeader from '@/components/ceo/CeoHeader.vue'
 export default {
+    components: {
+        CeoHeader,
+    }
 
 }
 </script>
