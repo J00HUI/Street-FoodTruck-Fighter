0591e48296b2e637e15ec821afea9fd902e7884b
ggpp200 <ggpp20000@gmail.com>
2022-10-28 13:38:09 +0900

    feat: 🎸 #21 사장님 footer 초본 디자인 완성

--- frontend/src/components/ceo/CeoHeader.vue
+++ frontend/src/components/ceo/CeoHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="header2">
-    <img src="@/assets/ceo/nav2Back.svg" alt />
+    <button @click ="goBack">
+      <img src="@/assets/ceo/nav2Back.svg" alt />
+    </button>
     <img src="@/assets/markerIcon.svg" alt />
     <address style="margin:auto;   font-style: normal;">주소</address>
     <img src="@/assets/noticeIcon.svg" alt />
@@ -9,10 +11,26 @@
 </template>
 
 <script>
-export default {};
+export default {
+  setup() {
+    function goBack() {
+      window.history.back()
+    }
+    return {
+      goBack,
+    }
+  }
+};
 </script>
 
 <style scoped>
+button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background-color: transparent;
+  border: none;
+}
 .header2 {
   font-family: "SCoreDream";
   display: flex;
