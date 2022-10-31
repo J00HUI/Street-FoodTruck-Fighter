915f3e8485ebff8e24fa049a06f5c012e39e8eb2
ggpp200 <ggpp20000@gmail.com>
2022-10-31 09:01:43 +0900

    feat: 🎸 #22 매출페이지 보류

--- frontend/src/views/ceo/SalesStatisticsView.vue
+++ frontend/src/views/ceo/SalesStatisticsView.vue
@@ -19,7 +19,6 @@
 </template>
 
 <script>
-import Chart from "chart.js/auto";
 import CeoHeader from "@/components/ceo/CeoHeader.vue";
 import Today from "@/components/ceo/SalesToday.vue";
 import Footer from "@/components/ceo/CeoFooter.vue";
@@ -29,61 +28,7 @@ export default {
     Today,
     Footer
   },
-  setup() {
-    const labels = Utils.months({ count: 7 });
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          label: "My First Dataset",
-          data: [65, 59, 80, 81, 56, 55, 40],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-            "rgba(255, 205, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(201, 203, 207, 0.2)"
-          ],
-          borderColor: [
-            "rgb(255, 99, 132)",
-            "rgb(255, 159, 64)",
-            "rgb(255, 205, 86)",
-            "rgb(75, 192, 192)",
-            "rgb(54, 162, 235)",
-            "rgb(153, 102, 255)",
-            "rgb(201, 203, 207)"
-          ],
-          borderWidth: 1
-        }
-      ]
-    };
-    const config = {
-      type: "bar",
-      data: data,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    };
-    const chart = new Chart(ctx, {
-      type: "line",
-      data: data,
-      options: {
-        onClick: e => {
-          const canvasPosition = getRelativePosition(e, chart);
-
-          // Substitute the appropriate scale IDs
-          const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
-          const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
-        }
-      }
-    });
-  }
+    
 };
 </script>
 
