31fd60d115bdbb3172d846afc80b996d32320a06
이성훈 <lgshceo@naver.com>
2022-10-31 17:39:32 +0900

    feat: 🎸 OrderView - 진행중

--- frontend/src/components/customer/BackButtonHeader.vue
+++ frontend/src/components/customer/BackButtonHeader.vue
@@ -1,19 +1,56 @@
 <template>
-  <div class="container">
-    <div class="header">
-        <img src="@/assets/Back-Navs.svg" style="width:2.5rem;" alt />
-        <img src="@/assets/Home.svg" style="width:2.5rem;" alt />
-        <img src="@/assets/Cart.svg" style="width:2.5rem;" alt />
+  <header class="header">
+    <div class="back">
+      <button @click ="goBack">
+        <img src="@/assets/Back-Navs.svg" alt />
+      </button>
     </div>
-  </div>
+    <div class="home">
+      <button @click ="goHome">
+        <img src="@/assets/Home.svg" alt />
+      </button>
+      <button @click ="goCart">
+        <img src="@/assets/Cart.svg" style="margin-left:1rem;" alt />
+      </button>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-
-}
+  setup() {
+    function goBack() {
+      window.history.back()
+    }
+    return {
+      goBack,
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background-color: transparent;
+  border: none;
+  align-items: left;
+}
+.header {
+  font-family: "SCoreDream";
+  display: flex;
+  width: 90%;
+  margin: 5%;
+  justify-content: space-between;
+  align-items: center;
+}
+.back{
+  align-self: left;
+}
+.home{
+  align-self: right;
+}
 
 </style>
