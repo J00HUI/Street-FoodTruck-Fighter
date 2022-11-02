d205c2bba429839b69a7c702e135cf3f0bfa8869
ggpp200 <ggpp20000@gmail.com>
2022-11-02 09:43:34 +0900

    feat: 🎸 #22 chart.js 인스톨 후 머지(디자인 안함)

--- frontend/src/components/ceo/SurveyKakaoMap.vue
+++ frontend/src/components/ceo/SurveyKakaoMap.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="ceo-survey-map"></div>
+  <div id="ceo-survey-map"></div>
 </template>
 
 <script>
@@ -26,23 +26,27 @@ export default {
 
       const options = {
         center: new kakao.maps.LatLng(36.36880618678187, 127.37618869404398),
-        level: 5
+        level: 5,
       };
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var moveLatLng = new kakao.maps.LatLng(position.coords.latitude, position.coords.longitude);   
+        navigator.geolocation.getCurrentPosition(function (position) {
+          var moveLatLng = new kakao.maps.LatLng(
+            position.coords.latitude,
+            position.coords.longitude
+          );
           initMap.map.panTo(moveLatLng);
-      })}
+        });
+      }
       initMap.map = new kakao.maps.Map(container, options);
       var clusterer = new kakao.maps.MarkerClusterer({
         map: initMap.map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
         averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
         minLevel: 1, // 클러스터 할 최소 지도 레벨
-        disableClickZoom: true // 클러스터 마커를 클릭했을 때 지도가 확대되지 않도록 설정한다
+        disableClickZoom: true, // 클러스터 마커를 클릭했을 때 지도가 확대되지 않도록 설정한다
       });
-      var markers = $(store.s_markers_info).map(function(i, item) {
+      var markers = $(store.s_markers_info).map(function (i, item) {
         return new kakao.maps.Marker({
-          position: new kakao.maps.LatLng(item[1], item[2])
+          position: new kakao.maps.LatLng(item[1], item[2]),
         });
       });
       clusterer.addMarkers(markers);
@@ -50,28 +54,30 @@ export default {
       // 마커 클러스터러에 클릭이벤트를 등록합니다
       // 마커 클러스터러를 생성할 때 disableClickZoom을 true로 설정하지 않은 경우
       // 이벤트 헨들러로 cluster 객체가 넘어오지 않을 수도 있습니다
-      kakao.maps.event.addListener(clusterer, "clusterclick", function(
-        cluster
-      ) {
-        console.log(cluster.getMarkers());
-        // 현재 지도 레벨에서 1레벨 확대한 레벨
-        var level = initMap.map.getLevel() - 1;
+      kakao.maps.event.addListener(
+        clusterer,
+        "clusterclick",
+        function (cluster) {
+          console.log(cluster.getMarkers());
+          // 현재 지도 레벨에서 1레벨 확대한 레벨
+          var level = initMap.map.getLevel() - 1;
 
-        // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
-        initMap.map.setLevel(level, { anchor: cluster.getCenter() });
-      });
+          // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
+          initMap.map.setLevel(level, { anchor: cluster.getCenter() });
+        }
+      );
     };
     return {};
-  }
+  },
 };
 </script>
 
 <style scoped>
 #ceo-survey-map {
   box-sizing: border-box;
-  width:90%;
-  height:76%;
-  margin:0px;
+  width: 90%;
+  height: 76%;
+  margin: 0px;
   border-radius: 1rem;
   margin: 5%;
 }
