ba0814ed0086f37b726b3547e5a4bcd4eaf2ab95
안태환 <atj14@naver.com>
2022-11-03 10:12:16 +0900

    feat: 🎸 #31 메인 UI 구현

---
+++ frontend/src/components/customer/WantFoodtruck.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="text-align: center">
+    <img
+      src="@/assets/wantfoodtruck.svg"
+      style="width: 85%; margin-top: 7%"
+      alt
+    />
+  </div>
+</template>
+      
+      <script>
+export default {};
+</script>
+    
+<style scoped>
+</style>
+      
