ee69726c5d5b240177d624f91c1a9a18afe8824d
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-11-04 11:11:56 +0900

    feat: #25 OrdersService - 주문 수락 추가

--- foodtruck/src/main/java/com/ssafy/foodtruck/model/service/OrdersService.java
+++ foodtruck/src/main/java/com/ssafy/foodtruck/model/service/OrdersService.java
@@ -1,13 +1,32 @@
 package com.ssafy.foodtruck.model.service;
 
-import com.ssafy.foodtruck.db.entity.User;
-import com.ssafy.foodtruck.dto.RegisterOrdersRequest;
+import com.ssafy.foodtruck.db.entity.*;
+import com.ssafy.foodtruck.db.repository.OrdersMenuRepository;
+import com.ssafy.foodtruck.db.repository.OrdersRepository;
+import com.ssafy.foodtruck.dto.*;
+import com.ssafy.foodtruck.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class OrdersService {
-    public void registerOrders(User user, RegisterOrdersRequest registerOrdersRequest) {
+
+    private final OrdersRepository ordersRepository;
+    private final OrdersMenuRepository ordersMenuRepository;
+
+    public void registerOrders(int userId, RegisterOrdersRequest registerOrdersRequest) {
+
+    }
+
+    @Transactional
+    public void acceptOrders(int userId, int ordersId) {
+        Orders orders = ordersRepository.findById(ordersId)
+                .orElseThrow(() -> new NotFoundException(OrdersErrorMessage.NOT_FOUND_MENU));
+        orders.setIsAccepted(true);
     }
 }
