0c922a7c1c40f9ee42b1c03948f9dbe342ff545d
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-11-04 10:55:37 +0900

    feat: #25 OrdersListByFoodtruckResponse 추가

---
+++ foodtruck/src/main/java/com/ssafy/foodtruck/dto/OrdersListByFoodtruckResponse.java
@@ -0,0 +1,12 @@
+package com.ssafy.foodtruck.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrdersListByFoodtruckResponse {
+
+    private String menuName;
+}
