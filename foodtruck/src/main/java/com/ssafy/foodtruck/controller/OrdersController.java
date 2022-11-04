978034465b0bb6be5810e01e21b003cc5311e627
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-11-04 10:45:00 +0900

    feat: #25 OrdersController - 주문 수락 추가

--- foodtruck/src/main/java/com/ssafy/foodtruck/controller/OrdersController.java
+++ foodtruck/src/main/java/com/ssafy/foodtruck/controller/OrdersController.java
@@ -1,5 +1,8 @@
 package com.ssafy.foodtruck.controller;
 
+import com.ssafy.foodtruck.dto.CurrentOrdersListByFoodtruckResponse;
+import com.ssafy.foodtruck.dto.OrdersHistoryResponse;
+import com.ssafy.foodtruck.dto.OrdersListByFoodtruckResponse;
 import com.ssafy.foodtruck.dto.RegisterOrdersRequest;
 import com.ssafy.foodtruck.model.service.OrdersService;
 import com.ssafy.foodtruck.model.service.UserService;
@@ -9,11 +12,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 import static com.ssafy.foodtruck.db.entity.Message.AUTHORIZATION;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/order")
+@RequestMapping("/order")
 public class OrdersController {
 
     private final UserService userService;
@@ -23,8 +28,15 @@ public class OrdersController {
     private final OrdersService ordersService;
 
     @PostMapping("/customer")
-    public ResponseEntity<?> registerOrders(@RequestHeader(AUTHORIZATION) String bearerToken, @RequestBody RegisterOrdersRequest registerOrdersRequest) {
-        ordersService.registerOrders(userService.getUserByEmail(jwtTokenUtil.getEmailFromBearerToken(bearerToken)), registerOrdersRequest);
+//    public ResponseEntity<?> registerOrders(@RequestHeader(AUTHORIZATION) String bearerToken, @RequestBody RegisterOrdersRequest registerOrdersRequest) {
+    public ResponseEntity<?> registerOrders(@PathVariable int userId, @RequestBody RegisterOrdersRequest registerOrdersRequest) {
+        ordersService.registerOrders(userId, registerOrdersRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping("/ceo/{ordersId}")
+    public ResponseEntity<?> acceptOrders(@PathVariable int userId, @PathVariable int ordersId) {
+        ordersService.acceptOrders(userId, ordersId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
