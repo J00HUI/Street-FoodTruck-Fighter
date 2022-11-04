4eedbdfa696194a5bf2249d19f4d66b68e561aae
이성훈 <lgshceo@naver.com>
2022-11-04 16:25:13 +0900

    feat: 🎸 메뉴 상세설명

--- frontend/src/components/customer/OrderMenu.vue
+++ frontend/src/components/customer/OrderMenu.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="card" >
-      <div>
-          <img class="picture" src="@/assets/hamburger.svg" alt />
-      </div>
-      <div class="detail">
-          <div class="storeName">
-              <h3 @click="showDetail">맛있는 햄버거</h3>
-          </div>
-          <div class="orders">
-              <h4>7500원</h4>
-          </div>
-      </div>
-      <hr>
+  <div class="cardContainer">
+    <div class="card" @click="showDetail">
+        <div class="picture">
+            <img  src="@/assets/hamburger.svg" alt />
+        </div>
+        <div class="detail">
+            <div class="storeName">
+                <h3 >맛있는 햄버거</h3>
+            </div>
+            <div class="orders">
+                <h4>7500원</h4>
+            </div>
+        </div>
+        
+    </div>
   </div>
+  <hr>
 </template>
 
 <script>
@@ -32,6 +35,30 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+/***************** card style start ******************/
+.cardContainer{
+    display: flex;
+    justify-content: center;
+    margin-top: 1rem;
+    margin-bottom: 1.5rem;
+}
+.card{
+    display: flex;
+    justify-content: left;
+    flex-direction: row;
+    align-items: center;
+    width: 21.5rem;
+    height: 6rem;
+    border: 0;
+}
+hr{
+  width: 100%;
+  color: lightgray;
+}
+.picture{
+  margin-right: 1rem;
+}
 
+/***************** card style end ******************/
 </style>
