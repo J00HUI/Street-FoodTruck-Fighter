13879c7198cc2850a729e7bf5d16cad0e3853512
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-11-04 10:55:02 +0900

    feat: #25 OrdersErrorMessage - 에러메세지 추가

--- foodtruck/src/main/java/com/ssafy/foodtruck/db/entity/OrdersErrorMessage.java
+++ foodtruck/src/main/java/com/ssafy/foodtruck/db/entity/OrdersErrorMessage.java
@@ -7,5 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum OrdersErrorMessage {
 
+    FAIL_TO_REGISTER("주문내역 등록에 실패하였습니다."),
+    NOT_FOUND_MENU("메뉴를 찾을 수 없습니다.."),
+    NOT_FOUND_USER("사용자를 찾을 수 없습니다.");
+
     private final String message;
 }
