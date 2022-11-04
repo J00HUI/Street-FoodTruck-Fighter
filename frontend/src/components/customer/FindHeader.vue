5b40aa948b47cc89a5874518cd1d71972720e339
안태환 <atj14@naver.com>
2022-11-04 16:33:17 +0900

    feat: 🎸 #38 지도 탭 UI 구현

---
+++ frontend/src/components/customer/FindHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="header2">
+    <button @click="goBack">
+      <img src="@/assets/ceo/nav2Back.svg" alt />
+    </button>
+    <img src="@/assets/markerIcon.svg" alt />
+    <address style="margin: auto; font-style: normal">주소</address>
+    <img src="@/assets/noticeIcon.svg" alt />
+    <img src="@/assets/humanIcon.svg" style="margin-left: 1rem" alt />
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    function goBack() {
+      window.history.back();
+    }
+    return {
+      goBack,
+    };
+  },
+};
+</script>
+
+<style scoped>
+button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background-color: transparent;
+  border: none;
+}
+.header2 {
+  font-family: "SCoreDream";
+  display: flex;
+  width: 90%;
+  margin: 5%;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- frontend/src/views/ceo/StartView.vue
+++ frontend/src/views/ceo/StartView.vue
@@ -1,34 +1,40 @@
 <template>
-    <div style="display: flex; height:844px;
-    flex-direction: column;">
+  <div style="display: flex; height: 844px; flex-direction: column">
     <CeoHeader></CeoHeader>
     <NewOrder></NewOrder>
-    <Order style="overflow-y: scroll; height: auto; min-height:10px"></Order>
-</div>
+    <Order class="orderScroll"></Order>
+  </div>
 </template>
 
 <script>
-import CeoHeader from '@/components/ceo/CeoHeader.vue'
-import NewOrder from '@/components/ceo/StartNewOrder.vue'
-import Order from '@/components/ceo/StartOrder.vue'
-import { onMounted } from 'vue'
+import CeoHeader from "@/components/ceo/CeoHeader.vue";
+import NewOrder from "@/components/ceo/StartNewOrder.vue";
+import Order from "@/components/ceo/StartOrder.vue";
+import { onMounted } from "vue";
 export default {
-    components: {
-        CeoHeader,
-        NewOrder,
-        Order,
-    },
-    setup() {
-        onMounted(() => {
-        })
+  components: {
+    CeoHeader,
+    NewOrder,
+    Order,
+  },
+  setup() {
+    onMounted(() => {});
 
-        return {
-        }
-    }
-
-}
+    return {};
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.orderScroll {
+  overflow-y: scroll;
+  height: auto;
+  min-height: 10px;
 
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+.orderScroll::-webkit-scrollbar {
+  display: none; /* Chrome, Safari, Opera*/
+}
 </style>
