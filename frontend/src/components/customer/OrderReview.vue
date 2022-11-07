92eb9a0f9c5b0f3c0f93a6f4a52c7a09010cddf4
이성훈 <lgshceo@naver.com>
2022-11-07 14:40:23 +0900

    fix: 🐛 indent refactor

--- frontend/src/components/customer/OrderReview.vue
+++ frontend/src/components/customer/OrderReview.vue
@@ -1,70 +1,66 @@
 <template>
   <div>
     <div class="cardContainer">
-        <div class="card">
-            <!-- 사진 -->
-            <div class="picture">
-              <img src="@/assets/Burger.svg" alt />
-            </div>
-            <hr>
-            <div class="detail">
-              <!-- 이름과 별점 -->
-              <div class="writer">
-                
-                  <h3>name</h3>
-                  <p>star</p>
-                
-              </div>
-              <!-- 리뷰 -->
-              <div class="review">
-                <p>너무 맛있는 인생맛집 발견 ㅠㅠㅠㅠ</p>
-              </div>
-            </div>
+      <div class="card">
+        <!-- 사진 -->
+        <div class="picture">
+          <img src="@/assets/Burger.svg" alt />
         </div>
+        <hr />
+        <div class="detail">
+          <!-- 이름과 별점 -->
+          <div class="writer">
+            <h3>name</h3>
+            <p>star</p>
+          </div>
+          <!-- 리뷰 -->
+          <div class="review">
+            <p>너무 맛있는 인생맛집 발견 ㅠㅠㅠㅠ</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
 /* card style start */
-.cardContainer{
-    display: flex;
-    justify-content: center;
-    margin-top: 1rem;
-    margin-bottom: 1.5rem;
+.cardContainer {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  margin-bottom: 1.5rem;
 }
-.card{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    width: 21.5rem;
-    height: 17rem;
-    border: 0.1rem solid #ffffff;
-    border-radius: 1rem;
-    box-shadow: 2px 3px 15px -6px grey;
+.card {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 21.5rem;
+  height: 17rem;
+  border: 0.1rem solid #ffffff;
+  border-radius: 1rem;
+  box-shadow: 2px 3px 15px -6px grey;
 }
-.picture{
+.picture {
   height: 13rem;
 }
-hr{
+hr {
   width: 100%;
   color: yellow;
 }
-.writer{
+.writer {
   display: flex;
   justify-content: space-around;
   flex-direction: row;
   align-items: left;
   height: 2.5rem;
 }
-.review{
+.review {
   height: 30%;
   margin-top: 5rem;
 }
--- frontend/src/views/customer/OrderView.vue
+++ frontend/src/views/customer/OrderView.vue
@@ -1,92 +1,111 @@
 <template>
-<Header></Header>
-<div class="container">
+  <Header></Header>
+  <div class="container">
     <div class="truck">
-        <h1>가게 이름</h1>
-        <form name="starForm" id="starForm">
-            <fieldset>
-                <input type="radio" name="rating" value="5" id="rate1"><label for="rate1">⭐</label>
-                <input type="radio" name="rating" value="4" id="rate2"><label for="rate2">⭐</label>
-                <input type="radio" name="rating" value="3" id="rate3"><label for="rate3">⭐</label>
-                <input type="radio" name="rating" value="2" id="rate4"><label for="rate4">⭐</label>
-                <input type="radio" name="rating" value="1" id="rate5"><label for="rate5">⭐</label>
-            </fieldset>
-        </form>
+      <h1>가게 이름</h1>
+      <form name="starForm" id="starForm">
+        <fieldset>
+          <input type="radio" name="rating" value="5" id="rate1" /><label
+            for="rate1"
+            >⭐</label
+          >
+          <input type="radio" name="rating" value="4" id="rate2" /><label
+            for="rate2"
+            >⭐</label
+          >
+          <input type="radio" name="rating" value="3" id="rate3" /><label
+            for="rate3"
+            >⭐</label
+          >
+          <input type="radio" name="rating" value="2" id="rate4" /><label
+            for="rate4"
+            >⭐</label
+          >
+          <input type="radio" name="rating" value="1" id="rate5" /><label
+            for="rate5"
+            >⭐</label
+          >
+        </fieldset>
+      </form>
     </div>
     <div class="cardContainer">
-        <div class="card">
-            <h1>대기인원 : 8명</h1>
-            <h3>예상 시간 : 10분</h3>
-        </div>
+      <div class="card">
+        <h1>대기인원 : 8명</h1>
+        <h3>예상 시간 : 10분</h3>
+      </div>
     </div>
-
 
     <div class="tabmenu out-tabmenu">
-        <ul>
-            <li id="tab1" class="btnCon"> 
-                <input type="radio" checked name="tabmenu" id="tabmenu1" @click="menuClick">
-                <label for="tabmenu1">메뉴</label>
-                <div class="tabCon" >
-                </div>
-            </li>
-            <li id="tab2" class="btnCon"><input type="radio" name="tabmenu" id="tabmenu2" @click="infoClick">
-                <label for="tabmenu2">정보</label>
-                <div class="tabCon" >
-                </div>
-            </li>    
-            <li id="tab3" class="btnCon">
-                <input type="radio" name="tabmenu" id="tabmenu3" @click="reviewClick">
-                <label for="tabmenu3">리뷰</label>
-                <div class="tabCon" >
-                </div>
-            </li>
-        </ul>
+      <ul>
+        <li id="tab1" class="btnCon">
+          <input
+            type="radio"
+            checked
+            name="tabmenu"
+            id="tabmenu1"
+            @click="menuClick"
+          />
+          <label for="tabmenu1">메뉴</label>
+          <div class="tabCon"></div>
+        </li>
+        <li id="tab2" class="btnCon">
+          <input type="radio" name="tabmenu" id="tabmenu2" @click="infoClick" />
+          <label for="tabmenu2">정보</label>
+          <div class="tabCon"></div>
+        </li>
+        <li id="tab3" class="btnCon">
+          <input
+            type="radio"
+            name="tabmenu"
+            id="tabmenu3"
+            @click="reviewClick"
+          />
+          <label for="tabmenu3">리뷰</label>
+          <div class="tabCon"></div>
+        </li>
+      </ul>
     </div>
-</div>
+  </div>
 
-    <Menu v-if="store.data == 1"></Menu>
-    <Info v-if="store.data == 2"></Info>
-    <Review v-if="store.data == 3"></Review>
-
+  <Menu v-if="store.data == 1"></Menu>
+  <Info v-if="store.data == 2"></Info>
+  <Review v-if="store.data == 3"></Review>
 </template>
 
 <script>
-import Header from '@/components/customer/BackButtonHeader.vue'
-import Menu from '@/components/customer/OrderMenu.vue'
-import Info from '@/components/customer/OrderInfo.vue'
-import Review from '@/components/customer/OrderReview.vue'
-import { useMenuStore } from '@/stores/menu'
+import Header from "@/components/customer/BackButtonHeader.vue";
+import Menu from "@/components/customer/OrderMenu.vue";
+import Info from "@/components/customer/OrderInfo.vue";
+import Review from "@/components/customer/OrderReview.vue";
+import { useMenuStore } from "@/stores/menu";
 
 export default {
-    components: {
-        Header,
-        Menu,
-        Info,
-        Review,
-    },
-    setup () {
-        const store = useMenuStore()
-        function menuClick() {
-            store.data = 1
-        }
-        function infoClick() {
-            store.data = 2
-        }
-        function reviewClick() {
-            store.data = 3
-        }
-
-        return{
-            store,
-            menuClick,
-            infoClick,
-            reviewClick,
-
-        }
+  components: {
+    Header,
+    Menu,
+    Info,
+    Review,
+  },
+  setup() {
+    const store = useMenuStore();
+    function menuClick() {
+      store.data = 1;
     }
-    
+    function infoClick() {
+      store.data = 2;
+    }
+    function reviewClick() {
+      store.data = 3;
+    }
 
-}
+    return {
+      store,
+      menuClick,
+      infoClick,
+      reviewClick,
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -96,114 +115,117 @@ export default {
 } */
 
 /***************** truck info ******************/
-.truck{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    max-width: 100%;
+.truck {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  max-width: 100%;
 }
 /***************** card ******************/
-.cardContainer{
-    display: flex;
-    justify-content: center;
-    margin-top: 1rem;
-    margin-bottom: 1.5rem;
+.cardContainer {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  margin-bottom: 1.5rem;
 }
-.card{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    width: 21.5rem;
-    height: 8rem;
-    border: 0.1rem solid #ffdd2d;
-    border-radius: 1rem;
-    box-shadow: 2px 3px 15px -6px grey;
+.card {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 21.5rem;
+  height: 8rem;
+  border: 0.1rem solid #ffdd2d;
+  border-radius: 1rem;
+  box-shadow: 2px 3px 15px -6px grey;
 }
 
 /***************** tabs start ******************/
-ul{
-    list-style:none;
-    padding-left:0px;
+ul {
+  list-style: none;
+  padding-left: 0px;
 }
-.tabmenu{ 
-  width:100%; 
-  margin: 0 auto; 
-  position:relative; 
-}
-.tabmenu ul{
+.tabmenu {
+  width: 100%;
+  margin: 0 auto;
   position: relative;
 }
-.tabmenu ul li{
-  display:  inline-block;
-  width:33.33%; 
+.tabmenu ul {
+  position: relative;
+}
+.tabmenu ul li {
+  display: inline-block;
+  width: 33.33%;
   /* float:left;   */
-  text-align:center; 
-  background :#f9f9f9;
-  line-height:40px;
+  text-align: center;
+  background: #f9f9f9;
+  line-height: 40px;
 }
-.tabmenu label{
-  display:block;
-  width:100%; 
-  height:40px;
-  line-height:40px;
-}
-.tabmenu input{display:none;}
-.tabCon{
-  display:none; 
+.tabmenu label {
+  display: block;
   width: 100%;
-  text-align:left; 
+  height: 40px;
+  line-height: 40px;
+}
+.tabmenu input {
+  display: none;
+}
+.tabCon {
+  display: none;
+  width: 100%;
+  text-align: left;
   padding: 20px;
-  position:absolute; 
-  left:0; top:40px; 
-  box-sizing: border-box; 
+  position: absolute;
+  left: 0;
+  top: 40px;
+  box-sizing: border-box;
   /* border : 5px solid #f9f9f9; */
 }
-.tabmenu input:checked ~ label{
-  background:#ccc;
+.tabmenu input:checked ~ label {
+  background: #ccc;
   text-decoration-line: underline;
   text-underline-position: under;
   /* text-underline-offset: inherit; */
   /* border-bottom: 1px solid #ffdd2d; */
 }
-.tabmenu input:checked ~ .tabCon{
-  display:flex;
+.tabmenu input:checked ~ .tabCon {
+  display: flex;
 }
 /***************** tabs end ******************/
 
 /***************** 별점 start ******************/
-#starForm fieldset{
-    display: inline-block; /* 하위 별점 이미지들이 있는 영역만 자리를 차지함.*/
-    border: 0; /* 필드셋 테두리 제거 */
+#starForm fieldset {
+  display: inline-block; /* 하위 별점 이미지들이 있는 영역만 자리를 차지함.*/
+  border: 0; /* 필드셋 테두리 제거 */
 }
-#starForm input[type=radio]{
-    display: none; /* 라디오박스 감춤 */
+#starForm input[type="radio"] {
+  display: none; /* 라디오박스 감춤 */
 }
-#starForm label{
-    font-size: 3em; /* 이모지 크기 */
-    color: transparent; /* 기존 이모지 컬러 제거 */
-    text-shadow: 0 0 0 #f0f0f0; /* 새 이모지 색상 부여 */
+#starForm label {
+  font-size: 3em; /* 이모지 크기 */
+  color: transparent; /* 기존 이모지 컬러 제거 */
+  text-shadow: 0 0 0 #f0f0f0; /* 새 이모지 색상 부여 */
 }
-#starForm label:hover{
-    color:black; /* text 컬러 원상태로 */
-    opacity:0.5; /* text 투명도 50% */
+#starForm label:hover {
+  color: black; /* text 컬러 원상태로 */
+  opacity: 0.5; /* text 투명도 50% */
 }
-#starForm label:hover ~ label{
-    opacity:0.5;
-    color:black;
+#starForm label:hover ~ label {
+  opacity: 0.5;
+  color: black;
 }
-#starForm fieldset{
-    display: inline-block; /* 하위 별점 이미지들이 있는 영역만 자리를 차지함.*/
-    direction: rtl; /* 이모지 순서 반전 */
-    border: 1px; /* 필드셋 테두리 제거 */
+#starForm fieldset {
+  display: inline-block; /* 하위 별점 이미지들이 있는 영역만 자리를 차지함.*/
+  direction: rtl; /* 이모지 순서 반전 */
+  border: 1px; /* 필드셋 테두리 제거 */
 }
-#starForm fieldset legend{
-    text-align: left;
+#starForm fieldset legend {
+  text-align: left;
 }
-#starForm input[type=radio]:checked ~ label{
-    opacity:1; /* text 투명도 100% */
-    color:black;
+#starForm input[type="radio"]:checked ~ label {
+  opacity: 1; /* text 투명도 100% */
+  color: black;
 }
 /***************** 별점 end ******************/
 </style>
