bdc04e2f25837ab7773d464848d99a338e1d1afd
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-11-07 10:49:27 +0900

    feat: #47 CurrentOrdersHistoryRes - 클래스명 변경

--- foodtruck/src/main/java/com/ssafy/foodtruck/dto/CurrentOrdersHistoryRes.java
+++ foodtruck/src/main/java/com/ssafy/foodtruck/dto/CurrentOrdersHistoryRes.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class CurrentOrdersHistoryResponse {
+public class CurrentOrdersHistoryRes {
 
     private String foodtruckName;
     private String menuName;
