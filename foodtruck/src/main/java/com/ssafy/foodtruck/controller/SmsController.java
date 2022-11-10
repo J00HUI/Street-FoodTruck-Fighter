6edeed9879972008ac64d87910754ebfc675c79d
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-11-10 16:14:57 +0900

    feat: #68 SmsController 파라미터 수정

--- foodtruck/src/main/java/com/ssafy/foodtruck/controller/SmsController.java
+++ foodtruck/src/main/java/com/ssafy/foodtruck/controller/SmsController.java
@@ -1,14 +1,13 @@
 package com.ssafy.foodtruck.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.ssafy.foodtruck.dto.request.Request;
 import com.ssafy.foodtruck.dto.response.SmsResponse;
 import com.ssafy.foodtruck.model.service.SmsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
@@ -24,8 +23,8 @@ public class SmsController {
 	private final SmsService smsService;
 
 	@PostMapping("/sms")
-	public ResponseEntity<SmsResponse> test(@RequestBody Request request) throws NoSuchAlgorithmException, URISyntaxException, UnsupportedEncodingException, InvalidKeyException, JsonProcessingException {
-		SmsResponse data = smsService.sendSms(request.getRecipientPhoneNumber());
+	public ResponseEntity<SmsResponse> test(@RequestParam String phoneNumber) throws NoSuchAlgorithmException, URISyntaxException, UnsupportedEncodingException, InvalidKeyException, JsonProcessingException {
+		SmsResponse data = smsService.sendSms(phoneNumber);
 		return ResponseEntity.ok().body(data);
 	}
 }
