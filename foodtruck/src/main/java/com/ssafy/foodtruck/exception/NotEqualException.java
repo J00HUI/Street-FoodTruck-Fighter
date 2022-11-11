af52b9c61f7a75d22ce5edf4606ab4c386e03f7a
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-11-11 15:48:53 +0900

    feat: #68 NotEqualException - 컨벤션 수정

--- foodtruck/src/main/java/com/ssafy/foodtruck/exception/NotEqualException.java
+++ foodtruck/src/main/java/com/ssafy/foodtruck/exception/NotEqualException.java
@@ -3,6 +3,7 @@ package com.ssafy.foodtruck.exception;
 import com.ssafy.foodtruck.db.entity.OrdersErrorMessage;
 
 public class NotEqualException extends RuntimeException {
+
 	public NotEqualException(OrdersErrorMessage message) {
 		super(message.getMessage());
 	}
