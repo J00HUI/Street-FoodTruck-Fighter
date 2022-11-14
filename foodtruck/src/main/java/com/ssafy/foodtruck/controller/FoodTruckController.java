846d256d8ca776da3bc6813a7e4497f366704af3
joohui <joohuii96@gmail.com>
2022-11-14 16:33:35 +0900

    refactor: 💡 #82 - 필요없는 import 삭제

--- foodtruck/src/main/java/com/ssafy/foodtruck/controller/FoodTruckController.java
+++ foodtruck/src/main/java/com/ssafy/foodtruck/controller/FoodTruckController.java
@@ -20,8 +20,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.xml.ws.Response;
-
 import static com.ssafy.foodtruck.constant.FoodTruckConstant.*;
 
 import java.io.IOException;
