1aa31a97cb04ff6b17e72aff72c961d035d6fb8a
smilejun <godhkekf24@inha.edu>
2022-11-19 16:10:19 +0900

    refactor: 💡 #113 결산 확인 오류 수정 (진행중, DB 설정 한번 바꿀 예정)

--- foodtruck/src/main/java/com/ssafy/foodtruck/model/service/FoodTruckService.java
+++ foodtruck/src/main/java/com/ssafy/foodtruck/model/service/FoodTruckService.java
@@ -201,7 +201,7 @@ public class FoodTruckService {
 			.orgNm(origName)
 			.savedNm(savedName)
 			.savedPath(savedPath)
-			.foodTruck(foodTruck.get())
+//			.foodTruck(foodTruck.get())
 			.build();
 
 		// 실제로 로컬에 uuid를 파일명으로 저장
