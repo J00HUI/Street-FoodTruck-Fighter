ba2e76b66bef4439c8ea4ac84e3232ca0c20f2ed
이성훈 <lgshceo@naver.com>
2022-11-21 07:15:34 +0900

    fix: 🐛 pay views fix

--- frontend/src/components/customer/PayFail.vue
+++ frontend/src/components/customer/PayFail.vue
@@ -1,18 +1,21 @@
 <template>
+  <div class="container">
     <div class="header">
-        <button @click ="goBack">
-            <img src="@/assets/CloseButton.svg" alt />
+      <router-link to="/">
+        <button>
+          <img src="@/assets/CloseButton.svg" alt />
         </button>
-        <div>
-            <h2>주문 실패</h2>
-        </div>
-
+      </router-link>
+      <div>
+        <h2>주문 실패</h2>
+      </div>
     </div>
-    <div class="container">
-        <div class="pic">
-            <img src="@/assets/PayFail.svg" alt />
-        </div>
+    <div class="picContainer">
+      <div class="pic">
+        <img src="@/assets/PayFail.svg" alt />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -29,28 +32,44 @@ export default {
 </script>
 
 <style>
-.header{
-    display: flex;
-    flex-direction: row;
-    height: 1.5rem;
-    width: 100%;
-}
-button{
-    margin-left: 1rem;
-}
-h2{
-    margin-left: 1.5rem;
-}
 .container{
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    height: 15rem;
-    width: 100%;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: space-around;
 }
-.pic{
-    width: 70%;
-    height: 50%;
+.header {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  height: 2.5rem;
+  width: 100%;
+  justify-content: center;
+  align-content: center;
+  margin-top: 1rem;
 }
-
+button {
+  position: absolute;
+  top: 1.2rem;
+  left: 1rem;
+  height: 80%;
+}
+h2 {
+  position: absolute;
+  top: 0.5rem;
+  left: 9rem;
+  height: 100%;
+}
+.picContainer {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  height: 15rem;
+  width: 100%;
+}
+.pic {
+  margin-top: 3rem;
+  width: 70%;
+  height: 50%;
+}
 </style>
